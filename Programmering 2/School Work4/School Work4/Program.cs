756c573a37284e26ae834eb98be0f1c15e64ca26
Rahm/Drett <mackan95@live.se>
2013-09-24 11:35:59 +0200

    Lade till början till 004
    Kommit en biut på vägen. Skapat klasser med mera.

---
+++ Programmering 2/School Work4/School Work4/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace School_Work4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person();
+            student s = new student();
+            Console.ReadKey();
+        }
+    }
+    class Person
+    {
+        private string name;
+        private string pnr;
+        private string telenr;
+        private string adress;
+
+        public string getname() { return name; }
+        public void setName(string name) { this.name = name; }
+
+        public string getPnr() { return pnr; }
+        public void setPnr(string personalNumber) { this.pnr = personalNumber; }
+
+        public string getTeleNr() { return telenr; }
+        public void setTeleNr(string telenr) { this.telenr = telenr; }
+
+        public string getAdress() { return adress; }
+        public void setAdress(string Adress) { this.adress = Adress; }
+    }
+    class student : Person
+    {
+        public student()
+        {
+
+        }
+        public student(string name, string pnr, string telenr, string adress)
+        {
+            setName(name);
+            setPnr(pnr);
+            setTeleNr(telenr);
+            setAdress(adress);
+        }
+        
+    }
+    class teacher : Person
+    {
+        public teacher()
+        {
+
+        }
+        public teacher(string name, string pnr, string telenr, string adress)
+        {
+            setName(name);
+            setPnr(pnr);
+            setTeleNr(telenr);
+            setAdress(adress);
+        }
+    }
+    class Class
+    {
+       
+    }
+}
